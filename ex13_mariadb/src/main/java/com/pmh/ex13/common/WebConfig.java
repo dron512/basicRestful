2fbd5ced83d9f6592c5099f91e7fb65484b2090c
dron512 <parkmyounghoi@gmail.com>
2025-07-30 14:49:20 +0900

    no message

---
+++ ex13_mariadb/src/main/java/com/pmh/ex13/common/WebConfig.java
@@ -0,0 +1,36 @@
+package com.pmh.ex13.common;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        WebMvcConfigurer.super.addInterceptors(registry);
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login",
+                        "/user/insert",
+                        "/swagger-ui/**",
+                        "/api-docs/**",
+                        "/v3/api-docs/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .maxAge(3600);
+    }
+}
---
+++ ex13_mariadb/src/test/java/com/pmh/ex13/Ex10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmh.ex13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
